6e812aba92c7281cf71922db8724fc84a3288b36
jaybobo <info@goodproduce.net>
2014-10-11 12:02:25 -0400

    add fields to url migration

--- source/db/migrate/20141011153127_create_urls.rb
+++ source/db/migrate/20141011153127_create_urls.rb
@@ -2,6 +2,9 @@ class CreateUrls < ActiveRecord::Migration
   def change
     create_table :urls do |t|
       t.string :full_url
+      t.string :short_url
+      t.integer :click_counter
+      t.integer :user_id
 
       t.timestamps
     end

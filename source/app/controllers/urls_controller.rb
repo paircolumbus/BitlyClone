101767b6fedf8222644942753d1cedff6787de10
degoeym <degoeym@gmail.com>
2014-10-11 12:49:03 -0400

    merge

--- source/app/controllers/urls_controller.rb
+++ source/app/controllers/urls_controller.rb
@@ -1,7 +1,13 @@
 class UrlsController < ApplicationController
 
+<<<<<<< Updated upstream
   def create
     @url = Url.new(params[:url])
     @url.save
   end
 end
+=======
+	def list
+	end
+end
+>>>>>>> Stashed changes

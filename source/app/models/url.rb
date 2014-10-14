d57d0356533ff332edc4148da41aa301125a6b90
degoeym <degoeym@gmail.com>
2014-10-13 23:23:38 -0400

    Changed short URL generator

--- source/app/models/url.rb
+++ source/app/models/url.rb
@@ -5,7 +5,7 @@ class Url < ActiveRecord::Base
   private
 
   def generate_shortened_url(length=6)
-    charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
+  	charset = [*('a'..'z'),*('A'..'Z'),*('0'..'9')]
     (0...length).map{ charset.to_a[rand(charset.size)] }.join
   end
 

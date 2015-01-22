23e7575630fbb75e1de1704beb9d0647c49cad3c
Nicolas McCurdy <thenickperson@gmail.com>
2015-01-22 15:07:52 -0500

    Fix tests (with 100% coverage)

--- source/test/controllers/urls_controller_test.rb
+++ source/test/controllers/urls_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UrlsControllerTest < ActionController::TestCase
   setup do
-    @url = urls(:one)
+    @valid_url = urls :valid
+    @invalid_url = urls :invalid
   end
 
   test "should get index" do
@@ -16,22 +17,36 @@ class UrlsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create url" do
+  test "should create valid url" do
     assert_difference('Url.count') do
-      post :create, url: { click_count: @url.click_count, long_url: @url.long_url, short_url: @url.short_url }
+      post :create, url: { click_count: @valid_url.click_count, long_url: @valid_url.long_url, short_url: @valid_url.short_url }
     end
 
-    assert_redirected_to url_path(assigns(:url))
+    assert_redirected_to urls_url
   end
 
-  test "should show url" do
-    get :show, id: @url
-    assert_response :success
+  test "should not create invalid url" do
+    assert_no_difference('Url.count') do
+      post :create, url: { click_count: @invalid_url.click_count, long_url: @invalid_url.long_url, short_url: @invalid_url.short_url }
+    end
   end
 
-  test "should destroy url" do
+  test "should show valid url" do
+    get :show, id: @valid_url
+    assert_response :redirect
+    assert_redirected_to @valid_url.long_url
+  end
+
+  test "should not show nonexistent url" do
+    assert_raise ActionController::RoutingError do
+      get :show, id: 'nonexistent'
+      assert_response :missing
+    end
+  end
+
+  test "should destroy valid url" do
     assert_difference('Url.count', -1) do
-      delete :destroy, id: @url
+      delete :destroy, id: @valid_url
     end
 
     assert_redirected_to urls_path

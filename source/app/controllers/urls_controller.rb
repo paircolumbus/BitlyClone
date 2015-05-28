4879ed17516c51885c7983e0e285f07d2356c90c
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-28 14:53:08 -0400

    Working release 1

--- source/app/controllers/urls_controller.rb
+++ source/app/controllers/urls_controller.rb
@@ -1,2 +1,16 @@
 class UrlsController < ApplicationController
+
+  def index
+    @urls = Url.all
+  end
+
+  def create
+    Url.create(original_url: params[:original_url])
+    render nothing: true
+  end
+
+  def show
+    @url = Url.find_by(short_path: params[:id])
+    redirect_to @url.original_url
+  end
 end

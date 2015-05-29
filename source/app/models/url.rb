4ac93535d2373696f7b1886e11b22970e58e4a77
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-29 12:18:22 -0400

    URL validations

--- source/app/models/url.rb
+++ source/app/models/url.rb
@@ -1,7 +1,27 @@
+require 'uri'
+
 class Url < ActiveRecord::Base
   before_save :create_short
+  validates :original_url, presence: true
+  validate :check_protocol, :uri_check, :can_be_reached
 
   private
+
+  def check_protocol
+    errors.add(:base, "URL must begin with http:// or https://") unless self.original_url =~ /^https?:\/\//
+  end
+
+  def uri_check
+    errors.add(:base, "Not a valid URL!") unless self.original_url =~ URI.regexp(['http', 'https'])
+  end
+
+  def can_be_reached
+    begin
+      HTTParty.get(self.original_url).code == 200
+    rescue
+      errors.add(:base, "Not reachable")
+    end
+  end
 
   def create_short
     self.short_path ||= SecureRandom.hex(3)

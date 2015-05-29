54c7f468e54e2bc4b641d2a2f792d04f880739c8
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-29 12:15:57 -0400

    Changed url from an instance variable

--- source/app/controllers/urls_controller.rb
+++ source/app/controllers/urls_controller.rb
@@ -10,8 +10,8 @@ class UrlsController < ApplicationController
   end
 
   def show
-    @url = Url.find_by(short_path: params[:id])
-    @url.update(click_count: @url.click_count + 1)
-    redirect_to @url.original_url
+    url = Url.find_by(short_path: params[:id])
+    url.update(click_count: url.click_count + 1)
+    redirect_to url.original_url
   end
 end

c27cd1c8dcee22c2e61f984a4b4beef2d898380b
Lovinder Pnag <lspnag@gmail.com>
2015-05-31 19:27:54 -0400

    working bitly clone with error handling

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   resources :urls
 
+  post "url/:short" => "urls#link", as: "link"
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 

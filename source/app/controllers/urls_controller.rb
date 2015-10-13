e74ead379d810646178a5d8509d5eb9d77306a84
Gabriel Escamilla <GEscamilla@covermymeds.com>
2015-10-13 15:22:40 -0400

    Basic tests and functionality for V0'

--- source/app/controllers/urls_controller.rb
+++ source/app/controllers/urls_controller.rb
@@ -1,2 +1,84 @@
 class UrlsController < ApplicationController
+  before_action :set_url, only: [:show, :edit, :update, :destroy]
+
+  # GET /urls
+  # GET /urls.json
+  def index
+    if params.has_key?(:alert)
+      flash.now[:alert] = params[:alert]
+    end
+    @urls = Url.all
+  end
+
+  # GET /urls/1
+  # GET /urls/1.json
+  def show
+  end
+
+  def short
+
+    url = url_from_params
+    if url == nil
+      redirect_to action: "index", alert: "Your URL was not found"
+    else
+      redirect_to url.original
+    end
+
+  end
+
+  def url_from_params
+    url = nil
+    if params.has_key?(:id)
+      url = Url.find_by_id(params[:id])
+    elsif params.has_key?(:unmatched_route)
+      url = Url.find_by_shortened("/#{params[:unmatched_route]}")
+    end
+    url
+  end
+
+  # GET /urls/new
+  def new
+    @url = Url.new
+  end
+
+
+  # GET /urls/1/edit
+  def edit
+  end
+
+  # POST /urls
+  def create
+    @url = Url.new(url_params)
+    if @url.save
+      redirect_to @url, notice: 'Url was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /urls/1
+  def update
+    if @url.update(url_params)
+      redirect_to @url, notice: 'Url was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /urls/1
+  def destroy
+    @url.destroy
+    redirect_to urls_url, notice: 'Url was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_url
+      @url = Url.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def url_params
+      params.require(:url).permit(:original, :shortened)
+    end
 end
---
+++ source/spec/controllers/urls_controller_spec.rb
@@ -0,0 +1,141 @@
+require 'rails_helper'
+
+RSpec.describe UrlsController, type: :controller do
+  URLS_PATH_WITH_OPTIONAL_QS = %r{\/urls\??.*}
+  VALID_URL_PATH = 'cmm'
+  INVALID_URL_PATH = 'somethingweird'
+
+  let(:valid_attributes) do
+    { original: 'http://www.covermymeds.com', shortened: "/#{VALID_URL_PATH}" }
+  end
+  let(:invalid_attributes) { {} }
+  let(:valid_session) { {} }
+
+  describe 'GET index' do
+    it 'assigns all urls as @urls' do
+      url = Url.create! valid_attributes
+      get :index, {}, valid_session
+      expect(assigns(:urls)).to eq([url])
+    end
+  end
+
+  describe 'GET index with error message' do
+    it 'assigns all urls as @urls' do
+      url = Url.create! valid_attributes
+      get :index, { alert: 'some error' }, valid_session
+      expect(assigns(:urls)).to eq([url])
+      expect(flash.now[:alert]).to be_present
+    end
+  end
+
+  describe 'GET show' do
+    it 'assigns the requested url as @url' do
+      url = Url.create! valid_attributes
+      get :show, { id: url.to_param }, valid_session
+      expect(assigns(:url)).to eq(url)
+    end
+  end
+
+  describe 'GET short with id' do
+    it 'redirect to the original url' do
+      url = Url.create! valid_attributes
+      get :short, { id: url.to_param }, valid_session
+      expect(response).to redirect_to(url.original)
+    end
+  end
+
+  describe 'GET short with shortened path' do
+    it 'redirect to the original url' do
+      url = Url.create! valid_attributes
+      get :short, { unmatched_route: VALID_URL_PATH }, valid_session
+      expect(response).to redirect_to(url.original)
+    end
+  end
+
+  describe 'GET short with invalid path will redirect to index' do
+    it 'redirect to the original url' do
+      get :short, { unmatched_route: INVALID_URL_PATH }, valid_session
+      expect(response).to redirect_to(URLS_PATH_WITH_OPTIONAL_QS)
+    end
+  end
+
+  describe 'GET new' do
+    it 'assigns a new url as @url' do
+      get :new, {}, valid_session
+      expect(assigns(:url)).to be_a_new(Url)
+    end
+  end
+
+  describe 'GET edit' do
+    it 'assigns the requested url as @url' do
+      url = Url.create! valid_attributes
+      get :edit, { id: url.to_param }, valid_session
+      expect(assigns(:url)).to eq(url)
+    end
+  end
+
+  describe 'POST create' do
+    describe 'with valid params' do
+      it 'creates a new Url' do
+        expect do
+          post :create, { url: valid_attributes }, valid_session
+        end.to change(Url, :count).by(1)
+      end
+
+      it 'assigns a newly created url as @url' do
+        post :create, { url: valid_attributes }, valid_session
+        expect(assigns(:url)).to be_a(Url)
+        expect(assigns(:url)).to be_persisted
+      end
+
+      it 'redirects to the created url' do
+        post :create, { url: valid_attributes }, valid_session
+        expect(response).to redirect_to(Url.last)
+      end
+    end
+  end
+
+  describe 'PUT update' do
+    describe 'with valid params' do
+      let(:new_value) { 'new value' }
+      let(:new_attributes)  do
+        { original: new_value, shortened: new_value }
+      end
+
+      it 'updates the requested url' do
+        url = Url.create! valid_attributes
+        put :update, { id: url.to_param, url: new_attributes }, valid_session
+        url.reload
+        expect(url.shortened).to eq(new_value)
+        expect(url.original).to eq(new_value)
+      end
+
+      it 'assigns the requested url as @url' do
+        url = Url.create! valid_attributes
+        put :update, { id: url.to_param, url: valid_attributes }, valid_session
+        expect(assigns(:url)).to eq(url)
+      end
+
+      it 'redirects to the url' do
+        url = Url.create! valid_attributes
+        put :update, { id: url.to_param, url: valid_attributes }, valid_session
+        expect(response).to redirect_to(url)
+      end
+    end
+  end
+
+  describe 'DELETE destroy' do
+    it 'destroys the requested url' do
+      url = Url.create! valid_attributes
+      expect do
+        delete :destroy, { id: url.to_param }, valid_session
+      end.to change(Url, :count).by(-1)
+    end
+
+    it 'redirects to the urls list' do
+      url = Url.create! valid_attributes
+      delete :destroy, { id: url.to_param }, valid_session
+      expect(response).to redirect_to(urls_url)
+    end
+  end
+end

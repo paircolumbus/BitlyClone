b8781f1fea3d3acc7ea21e0c0b0cdc338be16ef3
Nick Legg <nlegg@covermymeds.com>
2015-10-21 09:55:55 -0400

    Remove short_url field from form

--- source/spec/views/urls/edit.html.erb_spec.rb
+++ source/spec/views/urls/edit.html.erb_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "urls/edit", :type => :view do
   before(:each) do
     @url = assign(:url, Url.create!(
-      :short_url => "MyString",
       :real_url => "http://example.com"
     ))
   end
@@ -12,9 +11,6 @@ RSpec.describe "urls/edit", :type => :view do
     render
 
     assert_select "form[action=?][method=?]", url_path(@url), "post" do
-
-      assert_select "input#url_short_url[name=?]", "url[short_url]"
-
       assert_select "input#url_real_url[name=?]", "url[real_url]"
     end
   end
--- source/spec/views/urls/new.html.erb_spec.rb
+++ source/spec/views/urls/new.html.erb_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "urls/new", :type => :view do
   before(:each) do
     assign(:url, Url.new(
-      :short_url => "MyString",
       :real_url => "MyString"
     ))
   end
@@ -12,9 +11,6 @@ RSpec.describe "urls/new", :type => :view do
     render
 
     assert_select "form[action=?][method=?]", urls_path, "post" do
-
-      assert_select "input#url_short_url[name=?]", "url[short_url]"
-
       assert_select "input#url_real_url[name=?]", "url[real_url]"
     end
   end

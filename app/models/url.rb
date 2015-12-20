0e065cecb12a3e6c0edd6f0f10070b9b73ca7724
Joe Reich <jreich@covermymeds.com>
2015-12-20 15:05:38 -0500

    v2: first three validations

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,13 @@
+require 'uri'
+
 class Url < ActiveRecord::Base
 
   belongs_to :user
+
+  JOES_URL_REGEX = /\Ahttps?:\/\//
+  URL_REGEX = URI.regexp
+  validates :longText,  presence: true, 
+                        format: {with: URL_REGEX, message: "Not a valid URI"},
+                        format: {with: JOES_URL_REGEX, message: "Joe says no"}
 
 end

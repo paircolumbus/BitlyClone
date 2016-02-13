0152fd7369a9981d580bf9cbea64215a73e4206c
awanninger <awanninger@mail.com>
2016-02-13 05:47:16 -0500

    rolled validation logic into a private method

--- source/app/controllers/urls_controller.rb
+++ source/app/controllers/urls_controller.rb
@@ -9,7 +9,7 @@ class UrlsController < ApplicationController
 
   def create
     # validate that the user entered a url
-    if url_params[:address].size <= 3 || url_params[:address].to_s.match(/^.+\..+/).blank?
+    unless valid_url? 
       flash[:error] = "Hold up! You need to enter a valid address."
       redirect_to :root and return
     end
@@ -18,7 +18,8 @@ class UrlsController < ApplicationController
 
     respond_to do |format|
       begin
-        raise ActiveRecord::RecordNotUnique.new(@url) if Url.where(address: url_params[:address]).count > 0 
+        raise ActiveRecord::RecordNotUnique.new(@url) if 
+          Url.where(address: url_params[:address]).count > 0 
         format.html { render :index } if @url.save
       rescue ActiveRecord::RecordNotUnique 
         @url = Url.where(address: url_params[:address]).first
@@ -30,7 +31,8 @@ class UrlsController < ApplicationController
   def destroy
     @url.destroy
     respond_to do |format|
-      format.html { redirect_to urls_url, notice: 'Url was successfully destroyed.' }
+      format.html { redirect_to urls_url, 
+                    notice: 'Url was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -44,10 +46,8 @@ class UrlsController < ApplicationController
     params.require(:url).permit(:address, :url)
   end
 
-  def address_not_uniq
-    address = params[:address] || ''
-    address.gsub!(/https:\/\/|http:\/\//,'')
-    address.gsub!(/www\./,'')
-    Url.where(address: address).first.blank?
+  def valid_url?
+    url_params[:address].size <= 3 || 
+      url_params[:address].to_s.match(/^.+\..+/).blank?
   end
 end

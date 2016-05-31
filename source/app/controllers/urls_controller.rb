9a9b40e7f25178c7e8319beeb44372bc9bc26526
Jonah Roth <jroth@covermymeds.com>
2016-05-31 15:40:48 -0400

    Release 0 re-implemented to follow challenge instructions

--- source/app/controllers/urls_controller.rb
+++ source/app/controllers/urls_controller.rb
@@ -2,12 +2,17 @@ class UrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    @url = Url.build_for_url(params[:address])
+    @url = Url.create(url_params)
     render json: @url
   end
 
   def get
     @url = Url.find_by(:shortcode => params[:id])
     redirect_to @url.address
+  end
+
+  private
+  def url_params
+    params.permit(:address)
   end
 end
--- source/app/models/url.rb
+++ source/app/models/url.rb
@@ -1,24 +1,19 @@
 require 'digest/sha1'
 
 class Url < ActiveRecord::Base
-  def self.build_for_url(url)
-    @url = new(:address => url)
-    @url.shortcode = self.get_code(url)
-    @url.save
-    @url
-  end
+  before_save :set_shortcode
 
   protected
 
-    def self.get_code(str)
-      full = Digest::SHA1.hexdigest(str)
+    def set_shortcode
+      full = Digest::SHA1.hexdigest(self.address)
       start = 0
       code = full[start..(start+5)]
       loop do
-        break if !self.find_by(:shortcode => code) || (start + 5 >= full.length)
+        break if Url.find_by(:shortcode => code) || (start + 5 >= full.length)
         start += 1
         code = full[start..(start+5)]
       end
-      code
+      self.shortcode = code
     end
 end

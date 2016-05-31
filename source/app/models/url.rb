4d40871576c018e9b8ef2d736f54bce7fa51ffae
Jonah Roth <jroth@covermymeds.com>
2016-05-31 16:47:33 -0400

    Validates URLs but still can't show errors

--- source/app/models/url.rb
+++ source/app/models/url.rb
@@ -1,7 +1,9 @@
 require 'digest/sha1'
+require 'uri'
 
 class Url < ActiveRecord::Base
   before_save :set_shortcode
+  validate :is_url, on: :create
 
   protected
 
@@ -10,10 +12,19 @@ class Url < ActiveRecord::Base
       start = 0
       code = full[start..(start+5)]
       loop do
-        break if Url.find_by(:shortcode => code) || (start + 5 >= full.length)
+        break if !Url.find_by(:shortcode => code) || (start + 5 >= full.length)
         start += 1
         code = full[start..(start+5)]
       end
       self.shortcode = code
     end
+
+    def is_url
+      errors.add(:address, "is not a valid URL") unless !!URI.parse(address)
+    rescue URI::InvalidURIError
+      errors.add(:address, "is not a valid URL")
+    end
+
+
+
 end

6b4ecd90d494b28c28c9101548105fcd79876faf
Hari Dahal <HDahal@covermymeds.com>
2018-01-26 10:14:04 -0500

    release 2&3 - added url validations

--- source/app/models/url.rb
+++ source/app/models/url.rb
@@ -1,6 +1,10 @@
+require 'uri'
+require 'net/http'
+
 class Url < ActiveRecord::Base
   before_save :shorten_url
   validates :long_url, presence: true
+  validate :long_url_proper_format, :long_url_responds
 
   private
 
@@ -9,4 +13,22 @@ class Url < ActiveRecord::Base
       self.short_url = (0...7).map { [*'0'..'9', *'A'..'Z', *'a'..'z'].sample }.join
     end
   end
+
+  def long_url_proper_format
+    unless long_url.start_with? 'http://', 'https://'
+      errors.add(:long_url, 'not in a valid format. (must start with http:// or https://)')
+    end
+  end
+
+  def long_url_responds
+    link = URI.parse(long_url)
+    if link.is_a?(URI::HTTP) || link.is_a?(URI::HTTPS)
+      Net::HTTP.get_response(link)
+    else
+      errors.add(:long_url, 'must be reachable.')
+    end
+  rescue
+    errors.add(:long_url, 'must be reachable.')
+  end
+
 end

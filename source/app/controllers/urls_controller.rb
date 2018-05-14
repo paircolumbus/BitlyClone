0aa6d5a3639b7ccfa7fc627557337c3786aed5dd
Cara Kane <ckane@cmmc02vg1aug8wl.innova.local>
2018-05-14 16:21:46 -0400

    finished release 0

--- source/app/controllers/urls_controller.rb
+++ source/app/controllers/urls_controller.rb
@@ -1,2 +1,32 @@
 class UrlsController < ApplicationController
+
+  def index
+    @all = Url.all
+  end
+
+  def show
+    if params[:id].start_with?("!")
+        @url = Url.find_by(:short_url => "localhost:3000/urls/#{params[:id]}")
+        redirect_to "http://#{@url[:long_url]}"
+    else 
+      @url = Url.find(params[:id])
+      url_path(@url)
+    end
+  end
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.create(url_params)
+    redirect_to url_path(@url)
+  end
+
+
+
+  private
+    def url_params
+      params.require(:url).permit(:long_url)
+    end
 end
---
+++ source/app/models/url.rb
@@ -0,0 +1,7 @@
+class Url < ActiveRecord::Base
+  before_save :shorten_url
+
+  def shorten_url
+    self.short_url = "localhost:3000/urls/!#{(0..3).map{ rand(36).to_s(36) }.join}"
+  end
+end

a9ceccbcdb045cc05016a245bf0db73833e48d70
Andrew Bonner <abonner@covermymeds.com>
2018-05-15 15:47:59 -0400

    Add login and sign up links when logged out.

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new", as: "log_in"
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy", as: "log_out"
-  get "/signup", to: "users#new", as: "signup"
+  get "/signup", to: "users#new", as: "sign_up"
 
   get "/:short_url", to: "urls#show", as: "short_url"
 end

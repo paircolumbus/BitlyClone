f7d1013c2147ce64710499c5abbed4525e06a2a5
Andrew Bonner <abonner@covermymeds.com>
2018-05-15 13:25:37 -0400

    Add user routes.

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :urls, only: [:index, :new, :create]
+
   get "/:short_url", to: "urls#show", as: "short_url"
+
+  resources :users, only: [:new, :create, :show]
 end

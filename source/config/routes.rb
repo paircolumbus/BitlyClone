1610a6a50370a49b6d6f3c6beb6650d104c1d2ce
Andrew Bonner <abonner@covermymeds.com>
2018-05-15 15:43:19 -0400

    Create user login and logout.

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :urls, only: [:index, :new, :create]
+  resources :users, only: [:create, :show]
+
+  get "/login", to: "sessions#new", as: "log_in"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy", as: "log_out"
+  get "/signup", to: "users#new", as: "signup"
 
   get "/:short_url", to: "urls#show", as: "short_url"
-
-  resources :users, only: [:new, :create, :show]
 end

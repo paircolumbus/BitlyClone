4ea2a9e0e7e48c1bf1cab95b21296ee7ff118eb1
Raghav Raman <rraman@covermymeds.com>
2018-05-17 14:00:04 -0400

    WIP: Release 0

--- source/app/controllers/urls_controller.rb
+++ source/app/controllers/urls_controller.rb
@@ -1,2 +1,39 @@
 class UrlsController < ApplicationController
+
+
+	def index
+		@urls= Url.all
+	end
+
+  def create
+    @url = Url.new(url_params)
+
+    if !@url.save
+     flash[:error] = "Invalid url"
+    end
+
+    redirect_to urls_path
+  end
+
+	def new
+			@url = Url.new
+	end
+
+	def show
+		@url = Url.find_by_source(params[:id])
+		redirect_to @url.destination
+	end
+
+	def destroy
+		@url = Url.find_by_source(params[:id])
+		@url.destroy
+
+		redirect_to urls_path
+	end
+
+ 	private
+  	def url_params
+      params.require(:url).permit(:destination)
+    end
 end
+

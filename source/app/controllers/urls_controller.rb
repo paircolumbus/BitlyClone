dc5c09162cd6d0148b53d15a0585a090520a503a
Leslie Kornes <lkornes@covermymeds.com>
2018-05-16 23:43:33 -0400

    Release 0 - Simple Shortener.

--- source/app/controllers/urls_controller.rb
+++ source/app/controllers/urls_controller.rb
@@ -1,7 +1,9 @@
 class UrlsController < ApplicationController
 
   def index
-    @urls = Url.all
+    if Url.all.count > 0
+      @urls = Url.all
+    end
   end
 
   def new
@@ -10,15 +12,26 @@ class UrlsController < ApplicationController
 
   def create
     @url = Url.new(url_params)
+    if validate_url?
+      flash.now[:danger] = "Check your URL. . ."
+      render 'new'
+    else
+      @url.save
+      flash.now[:success] = "Your URL is SHORT"
+      redirect_to url_path(@url)
+    end
   end
 
   def show
-
+    @url = Url.find(params[:id])
   end
 
   private
   def url_params
-    params.require(:recipe).permit(:long_url)
+    params.require(:url).permit(:long_url)
   end
 
+  def validate_url?
+    @url.long_url.exclude?(".com") && @url.long_url.exclude?(".org") && @url.long_url.exclude?(".net")
+  end
 end
--- source/app/models/url.rb
+++ source/app/models/url.rb
@@ -1,2 +1,10 @@
 class Url < ActiveRecord::Base
+  before_save :trim_url
+
+  private
+
+  def trim_url
+    self.short_url = SecureRandom.hex(3)
+  end
+
 end
